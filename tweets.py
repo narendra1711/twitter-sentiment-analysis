7d6ff05fa1e1281ef33cdd1e75e1f4d3888243fe
narendra1711 <33336096+narendra1711@users.noreply.github.com>
2018-12-26 23:17:33 +0530

    Updated
    Included Pie Chart Feature

--- tweets.py
+++ tweets.py
@@ -1,49 +1,137 @@
-import tweepy           # To consume Twitter's API
-import pandas as pd     # To handle data
-import numpy as np      # For number computing
-import csv              # to perform operations on csv file
-import nltk
-# For plotting and visualization:
-from IPython.display import display
+import re 
+import tweepy 
+from tweepy import OAuthHandler 
+from textblob import TextBlob 
+
+class TwitterClient(object): 
+	''' 
+	Generic Twitter Class for sentiment analysis. 
+	'''
+	def __init__(self): 
+		''' 
+		Class constructor or initialization method. 
+		'''
+		# keys and tokens from the Twitter Dev Console 
+		consumer_key = 'mQb6IL5vhONJ2pwrkFuh58KkS'
+		consumer_secret = 'uEGi0CkPUe50ARcWkCaZg79iBe6vawPzz9ubEwWTfZdUAhuEDi'
+		access_token = '597013287-GcwjXgCCXxDIJaX9F3Ex1iTdSDKcoYtrA5IOfinx'
+		access_token_secret = 'DkkV8obw6ZXeXQn7gdcdpfHDUWecsrvVCjndEGIZPlYYv'
+
+		# attempt authentication 
+		try: 
+			# create OAuthHandler object 
+			self.auth = OAuthHandler(consumer_key, consumer_secret) 
+			# set access token and secret 
+			self.auth.set_access_token(access_token, access_token_secret) 
+			# create tweepy API object to fetch tweets 
+			self.api = tweepy.API(self.auth) 
+		except: 
+			print("Error: Authentication Failed") 
+
+	def clean_tweet(self, tweet): 
+		''' 
+		Utility function to clean tweet text by removing links, special characters 
+		using simple regex statements. 
+		'''
+		return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])(\w+:\/\/\S+)", " ", tweet).split()) 
+
+	def get_tweet_sentiment(self, tweet): 
+		''' 
+		Utility function to classify sentiment of passed tweet 
+		using textblob's sentiment method 
+		'''
+		# create TextBlob object of passed tweet text 
+		analysis = TextBlob(self.clean_tweet(tweet)) 
+		# set sentiment 
+		if analysis.sentiment.polarity > 0: 
+			return 'positive'
+		elif analysis.sentiment.polarity == 0: 
+			return 'neutral'
+		else: 
+			return 'negative'
+
+	def get_tweets(self, query, count = 10): 
+		''' 
+		Main function to fetch tweets and parse them. 
+		'''
+		# empty list to store parsed tweets 
+		tweets = [] 
+
+		try: 
+			# call twitter api to fetch tweets 
+			fetched_tweets = self.api.search(q = query, count = count) 
+
+			# parsing tweets one by one 
+			for tweet in fetched_tweets: 
+				# empty dictionary to store required params of a tweet 
+				parsed_tweet = {} 
+
+				# saving text of tweet 
+				parsed_tweet['text'] = tweet.text 
+				# saving sentiment of tweet 
+				parsed_tweet['sentiment'] = self.get_tweet_sentiment(tweet.text) 
+
+				# appending parsed tweet to tweets list 
+				if tweet.retweet_count > 0: 
+					# if tweet has retweets, ensure that it is appended only once 
+					if parsed_tweet not in tweets: 
+						tweets.append(parsed_tweet) 
+				else: 
+					tweets.append(parsed_tweet) 
+
+			# return parsed tweets 
+			return tweets 
+
+		except tweepy.TweepError as e: 
+			# print error (if any) 
+			print("Error : " + str(e)) 
+
+
+# creating object of TwitterClient Class 
+api = TwitterClient() 
+# calling function to get tweets 
+tweets = api.get_tweets(query = 'KCR', count = 50) 
+
+# picking positive tweets from tweets 
+ptweets = [tweet for tweet in tweets if tweet['sentiment'] == 'positive'] 
+
+ptweetsPercentage = 100*len(ptweets)/len(tweets)
+# percentage of positive tweets 
+print("Positive tweets percentage: {} %".format(100*len(ptweets)/len(tweets))) 
+# picking negative tweets from tweets 
+ntweets = [tweet for tweet in tweets if tweet['sentiment'] == 'negative'] 
+
+ntweetsPercentage = 100*len(ntweets)/len(tweets)
+# percentage of negative tweets 
+print("Negative tweets percentage: {} %".format(100*len(ntweets)/len(tweets))) 
+#print("Negative tweets percentage: {} %".format(100*len(ntweets)/len(tweets))) 
+# percentage of neutral tweets 
+neutweetsPercentage = 100*(len(tweets) - len(ntweets) - len(ptweets))/len(tweets)
+#print("Neutral tweets percentage: {} % ".format(100*len(tweets - ntweets - ptweets)/len(tweets))) 
+
+# printing first 5 positive tweets 
+print("\n\nPositive tweets:") 
+for tweet in ptweets[:10]: 
+	print(tweet['text']) 
+
+# printing first 5 negative tweets 
+print("\n\nNegative tweets:") 
+for tweet in ntweets[:10]: 
+	print(tweet['text']) 
+
+
+#Dashboard
 import matplotlib.pyplot as plt
-import seaborn as sns
-#%matplotlib inline
 
-consumer_key = "2jKfIrZgM9kJz9F4XhilBXgi1"
-consumer_secret = "nxtp2lKRJnX74Tq1Zh3Q24Ox0lh0B7XZGTd7tmQJDOjNR8uFIV"
-access_token = "1017464608431341568-JkagP3u8mIqM95nGafFOeUZpXkVy3F"
-access_secret = "k144fWX6blh4vcNRfhERXnhkvZcapgnojqg0PRJOIoOIx"
+# Pie chart, where the slices will be ordered and plotted counter-clockwise:
+labels = 'Positive', 'Neutral', 'Negative'
+sizes = [ptweetsPercentage, neutweetsPercentage, ntweetsPercentage]
+explode = (0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Positive Tweets')
 
-query='telangana elections'
-count=2000
-# We import our access keys:
-#from credentials import *    # This will allow us to use the keys as variables
+fig1, ax1 = plt.subplots()
+ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+        shadow=True, startangle=90)
+ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
-# API's setup:
-def twitter_setup():
-    # Authentication and access using keys:
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_secret) 
-
-    # Return API with authentication:
-    api = tweepy.API(auth)
-    return api
-
-# We create an extractor object:
-extractor = twitter_setup()
-searched_tweets = [status.full_text for status in tweepy.Cursor(extractor.search, q=query,lang='en',tweet_mode='extended').items(count)]
-
-# split into words
-from nltk.tokenize import word_tokenize
-tokens = [word_tokenize(tweet) for tweet in searched_tweets]
-
-# convert to lower case
-tokens = [str(w).lower() for w in tokens]
-
-# remove remaining tokens that are not alphabetic
-words = [word for word in tokens if word.isalpha()]
-# filter out stop words
-from nltk.corpus import stopwords
-stop_words = set(stopwords.words('english'))
-words = [w for w in words if not w in stop_words]
-print(words[:100])
+plt.show()
+    
